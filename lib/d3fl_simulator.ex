5c02644f380d3941ef6e82a71885c51841c6416e
up0pt <giftmath01au@gmail.com>
2023-11-08 15:12:12 +0900

    init

---
+++ lib/d3fl_simulator.ex
@@ -0,0 +1,18 @@
+defmodule D3flSimulator do
+  @moduledoc """
+  Documentation for `D3flSimulator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> D3flSimulator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/d3fl_simulator_test.exs
@@ -0,0 +1,8 @@
+defmodule D3flSimulatorTest do
+  use ExUnit.Case
+  doctest D3flSimulator
+
+  test "greets the world" do
+    assert D3flSimulator.hello() == :world
+  end
+end
