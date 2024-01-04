4bd6e661c981d13e24e35e8b77559b262f4a2f50
up0pt <giftmath01au@gmail.com>
2024-01-04 19:06:11 +0900

    訓練後モデルのaggregation用関数の追加

--- lib/d3fl_simulator/computer_node/ai_core.ex
+++ lib/d3fl_simulator/computer_node/ai_core.ex
@@ -1,3 +1,32 @@
 defmodule D3flSimulator.ComputerNode.AiCore do
-  
+  def train_model() do
+
+  end
+
+  def weighted_mean_model(map_a, map_b, rate_b) do
+    keys = Map.keys(map_a)
+    result_map = %{}
+    result_map = Enum.map(keys, fn key ->
+      value_a = Map.get(map_a, key)
+      v_a_bias = Map.get(value_a, "bias")
+      v_a_kernel = Map.get(value_a, "kernel")
+
+      value_b = Map.get(map_b, key)
+      v_b_bias = Map.get(value_b, "bias")
+      v_b_kernel = Map.get(value_b, "kernel")
+
+      result_map = Map.put(
+        result_map,
+        key,
+        %{"bias" => Nx.multiply(v_b_bias, rate_b)
+                    |> Nx.add(v_a_bias)
+                    |> Nx.divide(2),
+          "kernel" => Nx.multiply(v_b_kernel, rate_b)
+                    |> Nx.add(v_a_kernel)
+                    |> Nx.divide(2)}
+      )
+      result_map
+    end)
+    result_map
+  end
 end
