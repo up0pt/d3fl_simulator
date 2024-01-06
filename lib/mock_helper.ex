b3b8e1d064f4b6bd141f65838d83e4d8ae24c2f6
up0pt <giftmath01au@gmail.com>
2024-01-06 18:40:37 +0900

    mockの微修正

--- lib/mock_helper.ex
+++ lib/mock_helper.ex
@@ -24,14 +24,17 @@ defmodule MockHelper do
     CalculatorNode.send_model_via_ch(3, 2)
     CalculatorNode.send_model_via_ch(2, 1)
     Process.sleep(10000)
-    IO.inspect(CalculatorNode.get_info(2))
 
     CalculatorNode.train(1)
     CalculatorNode.train(2)
     CalculatorNode.train(3)
     Process.sleep(10000)
-    IO.inspect(CalculatorNode.get_info(2))
 
+    CalculatorNode.send_model_via_ch(1, 2)
+    CalculatorNode.send_model_via_ch(3, 2)
+    CalculatorNode.send_model_via_ch(2, 1)
+    Process.sleep(10000)
+    IO.inspect(CalculatorNode.get_info(2))
 
   end
 end
