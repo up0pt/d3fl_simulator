d6993020d3cfbb3519f8d0964df789f1e0bd3504
up0pt <giftmath01au@gmail.com>
2024-02-02 12:47:28 +0900

    add data_dir fun to mockhelper

--- lib/mock_helper.ex
+++ lib/mock_helper.ex
@@ -1,43 +1,20 @@
 defmodule MockHelper do
   require Logger
+  alias D3flSimulator.Data
 
-  alias D3flSimulator.CalculatorNode
-  alias D3flSimulator.Channel
-  alias D3flSimulator.Channel.InputQoS
+  def prepare_data_directory!(node_counts) do
+    data_directory_path =
+      Application.get_env(:d3fl_simulator, :data_directory_path) ||
+        raise """
+        You have to configure :data_directory_path in config.exs
+        ex) config :ping_pong_measurer_Zenohex, :data_directory_path, "path/to/directory"
+        """
 
-  @spec start_mock() :: :ok
-  def start_mock() do
-    CalculatorNode.start_link(%{model: %{}, data: "b", node_index: 1})
-    CalculatorNode.start_link(%{model: %{}, data: "d", node_index: 2})
-    CalculatorNode.start_link(%{model: %{}, data: "f", node_index: 3})
+    dt_string = Data.datetime_to_string(DateTime.utc_now())
+    directory_name = "date_#{dt_string}_CalculatorNodeNum_#{node_counts}"
+    data_directory_path = Path.join(data_directory_path, directory_name)
 
-    Channel.start_link({0, %InputQoS{send_node_id: 1, recv_node_id: 2, latency: 0, packetloss: 1}})
-    Channel.start_link({1, %InputQoS{send_node_id: 3, recv_node_id: 2, latency: 0}})
-    Channel.start_link({2, %InputQoS{send_node_id: 2, recv_node_id: 1, latency: 0}})
-
-    CalculatorNode.train(1, 0)
-    CalculatorNode.train(2, 0)
-    CalculatorNode.train(3, 0)
-    Process.sleep(10000)
-
-
-    Channel.change_inputQoS(%InputQoS{send_node_id: 3, recv_node_id: 2, latency: 5})
-    Channel.get_info(3, 2)
-    CalculatorNode.send_model_via_ch(1, 2)
-    CalculatorNode.send_model_via_ch(3, 2)
-    CalculatorNode.send_model_via_ch(2, 1)
-    Process.sleep(10000)
-
-    CalculatorNode.train(1, 20_000)
-    CalculatorNode.train(2, 20_000)
-    CalculatorNode.train(3, 20_000)
-    Process.sleep(10000)
-
-    CalculatorNode.send_model_via_ch(1, 2)
-    CalculatorNode.send_model_via_ch(3, 2)
-    CalculatorNode.send_model_via_ch(2, 1)
-    Process.sleep(10000)
-    IO.inspect(CalculatorNode.get_info(2))
-
+    File.mkdir_p!(data_directory_path)
+    data_directory_path
   end
 end
