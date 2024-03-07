3cfacdb05623f377c1fc47cfa7542b2b4c8ab040
up0pt <giftmath01au@gmail.com>
2024-03-07 13:17:35 +0900

    :observer を利用可能にした

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule D3flSimulator.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :observer, :wx, :runtime_tools]
     ]
   end
 
